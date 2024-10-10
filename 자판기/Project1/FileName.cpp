a9d9cba2b0a8ad8bd83e8c50451661ea18bafea3
2021010519 <cohidb@naver.com>
2024-10-10 16:23:11 +0900

    Report#1 Q2 자판기 문제 최종본 입니다.

--- 자판기/Project1/FileName.cpp
+++ 자판기/Project1/FileName.cpp
@@ -9,22 +9,21 @@ int main(void)
 	int rm; //remainder 줄임말//
 
 	printf("input Investment amount and Price of goods");
-	scanf_s("%d%d", &ch, &pay);
-	if (pay < ch) {
+	scanf_s("%d%d", &ch, &pay); //값을 입력받는다.//
+	if (pay < ch) { //if를 사용하여 투입한 값보다 물건값이 더 클 때 잔액이 부족하다는 것을 가르쳐 준다.//  
 		
 		rm = ch - pay;
 		//잔돈= 물건값-투입값//
 		printf("천원 %d장\n", rm / 1000);
-
+		//잔돈 1000으로 나눠 1000원짜리의 개수 값을 알아낸다// 
 		rm = rm % 1000;
-
+		//잔돈에 1000원짜리를 뺀 나머지 값을 저장한다//
 		printf("500원 %d장\n", rm / 500);
-
+		//잔돈 1000으로 나눠 500원짜리의 개수 값을 알아낸다// 
 		rm = rm % 500;
+		//잔돈에 500원짜리를 뺀 나머지 값을 저장한다//
 		printf("100원 %d장\n", rm / 100);
-
-		rm = rm % 100;
-		
+		//잔돈 100으로 나눠 100원짜리의 개수 값을 알아낸다// 
 	}
 	else {
 		printf("잔액이 부족합니다.");
