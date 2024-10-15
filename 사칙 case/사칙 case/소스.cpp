c84cdeb6dc8198e84ea911aaf948d7687f3df483
Sungmin Choi <cohidb@naver.com>
2024-10-15 16:42:14 +0900

    사칙 case

---
+++ 사칙 case/사칙 case/소스.cpp
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main()
+{
+	int x;
+	int y;
+	int end;
+	char ro;
+
+	printf("input (x¤±y :)");
+	scanf_s("%d %c %d", &x, &ro, sizeof(ro), &y);
+		switch (ro)
+		{
+			{case '+':
+				end = x + y;
+				break;
+			}
+			{case '-':
+				end = x - y;
+				break; }
+			{case '*':
+
+				end = x * y;
+				break; }
+			{case ' /':
+				end = x / y;
+				break; }
+			{default:
+				end = 0; }
+
+	}
+
+		printf("%d", end);
+
+	return 0;
+}
