05fe6846d1093e1a91ae0eb6b4469d1df3996689
Sungmin Choi <cohidb@naver.com>
2024-10-23 17:17:55 +0900

    yes do

---
+++ 공배 공약 do/공배 공약 do/yes i do.cpp
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int	main()
+{
+	int A; //초기 a값 저장//
+	int B; //초기 b값 저장//
+	int a;
+	int b;
+	int c;//a%b저장값//
+
+	printf("input a and b : ");
+	scanf_s("%d %d", &a, &b);
+	A = a;
+	B = b;
+
+	do //유클리드 호제법 a%b==r이라고 한다면 a=b로 b=r로 대체 후 b가 0이 될때까지 이과정을 반복한다면 a값은 최소공약수가 된다.//
+	{
+		c = b;
+		b = a % b;
+		a = c;
+	} while (b != 0);
+	printf("a와 b의 최대공약수는 %d입니다.", a);
+	printf("a와 b의 최소공배수는 %d입니다.", A * B / a); // A*B=최소공배수*최대공약수 즉, A*B/최소공배수= 최대
+
+
+	return 0;
+
+}
